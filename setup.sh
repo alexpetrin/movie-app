0ba1a24f3ce79e10a5c9b11642e985c1014412cf
alexpetrin <alexpetrin2@gmail.com>
2022-12-07 20:39:58 -0600

    update DATABASE_URL

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://taxelcudhhqnxd:dc358b506952de06e0bc5fdce0e712acc16e25150f37795b1bff6566f9452d7e@ec2-52-204-195-41.compute-1.amazonaws.com:5432/d24eik3sqk8s7"
 echo "setup.sh script executed successfully!"
